3a707edb16a9c111d4bd5135a1bbf2616a90c2a3
Apoorva-M15 <157487490+Apoorva-M15@users.noreply.github.com>
2024-02-15 23:56:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const questions = [
+    {
+        question: 'वह स्कूल जाता है',
+        correctAnswer: 'He goes to school.'
+    },
+    {
+        question: 'ट्रेन आ रही है',
+        correctAnswer: 'Train is arriving.'   
+    },
+    {
+        question: 'वह एक शरारती लड़का है',
+        correctAnswer: 'He is a naughty boy.'
+    },
+    {
+        question: 'जावास्क्रिप्ट एक महान भाषा है',
+        correctAnswer: 'JavaScript is a great language.'
+    },
+    {
+        question: 'वेब विकास महान है',
+        correctAnswer: 'Web Development is great.'
+    }
+];
+
+co
+
+
